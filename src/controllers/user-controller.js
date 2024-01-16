9e93c1a4b9193742f77685babbc35092a4b31ceb
Teja Juluri <julurisaiteja6789@gmail.com>
2024-01-16 19:52:42 +0530

    added forgotPassword functionality

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -2,11 +2,11 @@ const {StatusCodes}=require('http-status-codes')
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const { UserService }=require('../services')
-const {User} = require("../models")
-const {SuccessResponse,ErrorResponse}=require('../utils/common')
+const {User,Token} = require("../models")
+const {SuccessResponse,ErrorResponse, SendEmail}=require('../utils/common')
 const AppError = require('../utils/errors/app-error')
 const { ServerConfig }=require('../config')
-
+const crypto = require("crypto")
 
 // Generate Token
 const generateToken = (id) =>{
@@ -52,14 +52,12 @@ async function loginUser(req,res){
     try{
         const {email,password} = req.body
         const user= await User.findOne({email})
-        console.log(user)
         if(!user)
             throw new AppError(`User with ${email} doesnt exist,please sign in`,StatusCodes.BAD_REQUEST);
         const isPasswordCorrect = await bcrypt.compare(password,user.password)
         if(!isPasswordCorrect)
             throw new AppError(`Wrong Password,Please try again`,StatusCodes.BAD_REQUEST);
         const token = generateToken(user._id);
-        console.log(token)
         res.cookie("token",token,{
             path:"/",
             httpOnly: true,
@@ -70,7 +68,6 @@ async function loginUser(req,res){
         // Create a new object with the "password" field excluded
         const sanitizedData = (({ password, ...rest }) => rest)(user._doc);
         SuccessResponse.data=sanitizedData;
-        console.log(SuccessResponse)
         return res
                 .status(StatusCodes.OK)
                 .json(SuccessResponse)
@@ -99,10 +96,76 @@ async function logout(req,res){
     }catch(error){
         ErrorResponse.error=error;
         return res
-              .status(error.statusCode)
-              .json(ErrorResponse);  
+                 .status(error.statusCode)
+                 .json(ErrorResponse);  
     }
    
 }
 
-module.exports={createUser,loginUser,logout}
+
+async function forgotPassword(req,res){
+
+    try{
+    
+        const {email} = req.body
+        const user = await User.findOne({email})
+
+        if(!user)
+            throw new AppError(`User with ${email} doesn't exist,please provide valid email`,StatusCodes.NOT_FOUND);
+
+        let token = await Token.findOne({userId:user._id})
+
+        // delete if there is any token exists already
+        if(token)
+            await token.deleteOne()
+
+        // create reset token 
+        let resetToken = crypto.randomBytes(32).toString("hex") + user._id
+
+        // Hash Token before saving
+        const hashedToken = crypto.
+                                    createHash("sha256").
+                                    update(resetToken).
+                                    digest("hex")
+        
+         // Save Token to DB
+        await new Token({
+            userId : user._id,
+            token : hashedToken,
+            createdAt : Date.now(),
+            expiresAt : Date.now() + 30*60*1000
+        }).save()
+
+          // construct reset url
+        const resetUrl = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`
+
+        // construct reset url
+        const message =`
+        <h2>Hello ${user.name}</h2>
+        <p>You requested for a password reset</p>
+        <p>Click on the link below to change your password</p>
+        <p>The reset link is valid only for 30 minutes</p>
+        <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
+        <p>Regards.....</p>
+        <p>Bookmarking.com Team</p> 
+        `
+
+        const subject = "Password Reset Request"
+        const sendTo = user.email
+        const sendFrom = process.env.EMAIL_USER
+        await SendEmail(subject,message,sendTo,sendFrom)
+
+        SuccessResponse.data = "Reset email sent"
+        return res
+                  .status(StatusCodes.OK)
+                  .json(SuccessResponse);    
+    }catch(error){
+        ErrorResponse.error = error;
+        return res
+                  .status(error.statusCode)
+                  .json(ErrorResponse)
+    }
+}
+
+
+module.exports={createUser,loginUser,logout,forgotPassword}
--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -1,10 +1,12 @@
 const express =  require("express");
 const { UserController } = require("../../controllers");
-const {UserMiddlewares} = require("../../middlewares")
+const {UserMiddlewares} = require("../../middlewares");
+const protect = require("../../middlewares/auth-middleware");
 const router = express.Router();
 
 router.post("/register",UserMiddlewares.validateRegisterUser,UserController.createUser)
 router.post("/login",UserMiddlewares.validateCreateRequest,UserController.loginUser)
 router.get("/logout",UserController.logout)
+router.patch("/forgotPassword",protect,UserController.forgotPassword)
 
 module.exports=router;
